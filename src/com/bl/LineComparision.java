f4ab3ddbf2c733328eac17164e7e6d422caabd9f
riya40 <priyanunna20@gmail.com>
2021-12-14 23:10:23 +0530

    Added:Welcome message

---
+++ src/com/bl/LineComparision.java
@@ -0,0 +1,9 @@
+package com.bl;
+
+public class LineComparision {
+    public static void main(String args[]){
+        // Displaying the Welcome Message
+        System.out.println("welcome to Line Comparision Computation");
+    }
+
+}
