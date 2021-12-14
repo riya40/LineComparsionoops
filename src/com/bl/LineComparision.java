7b772032f97da35d164a38027b4b951773359435
riya40 <priyanunna20@gmail.com>
2021-12-14 23:34:12 +0530

    Added:Ccomaprision with equal method

--- src/com/bl/LineComparision.java
+++ src/com/bl/LineComparision.java
@@ -3,9 +3,12 @@ package com.bl;
 import java.util.Scanner;
 
 public class LineComparision {
-    double getline(){
-        int x1,x2,y1,y2;
-        double r;
+    public static void main(String args[]){
+        // Displaying the Welcome Message
+        System.out.println("welcome to Line Comparision Computation");
+
+        int x1,x2,y1,y2,p1,p2,q1,q2;
+        double r,res;
 
         System.out.println("enter first value");
         Scanner sc=new Scanner(System.in);
@@ -17,19 +20,32 @@ public class LineComparision {
         System.out.println("enter fourth point");
         y2=sc.nextInt();
 
+
         int a=x2-x1;
         int b=y2-y1;
-        r=(Math.sqrt(Math.pow(a, 2)+Math.pow(b, 2)));
-        System.out.println(r);
-        return r;
-    }
-    public static void main(String args[]){
-        // Displaying the Welcome Message
-        System.out.println("welcome to Line Comparision Computation");
+        //inputs for second line
+        System.out.println("enter first value");
+        p1=sc.nextInt();
+        System.out.println("enter second value");
+        p2=sc.nextInt();
+        System.out.println("enter third point");
+        q1=sc.nextInt();
+        System.out.println("enter fourth point");
+        q2=sc.nextInt();
 
-        System.out.println("calling line class");
-        LineComparision lineComparision=new LineComparision();
-        lineComparision.getline();
+        int c=p2-p1;
+        int d=q2-q1;
+
+        r=(Math.sqrt(Math.pow(a, 2)+Math.pow(b, 2)));
+        res=(Math.sqrt(Math.pow(c, 2)+Math.pow(d, 2)));
+        System.out.println(r);
+
+        //compare of the two lines
+        if(r==res){
+            System.out.println("equal");
+        }else{
+            System.out.println("not equal");
+        }
     }
 
 }
